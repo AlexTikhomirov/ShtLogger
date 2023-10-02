2b53df2ee57dc6595a81e5ea93e4901a4ee2f54e
Alexandr Tikhomirov <alexandrtikhomirov@gmail.com>
2023-10-02 21:39:13 +0200

    Started ShtLoggerDemo-iOS

---
+++ ShtLoggerDemo-iOS/ShtLoggerDemo-iOS/Logger/LoggerManager.swift
@@ -0,0 +1,16 @@
+import Foundation
+import ShtLogger
+
+let logger = ShtLogger.shared
+
+struct LoggerManager {
+    func setDefault() {
+        logger.logFileName = "ShtLogger Demo - iOS"
+        logger.isNotSave = false
+        logger.emails = ["shtroger@aol.com"]
+        logger.subTitleLogFile = ""
+        logger.dateFormat = "dd.MM.YYYY HH:mm:ss"
+        logger.timeFormat = "[HH:mm:ss]"
+        logger.countLogFiles = 5
+    }
+}
---
+++ ShtLoggerDemo-iOS/ShtLoggerDemo-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ShtLoggerDemo-iOS
+//
+//  Created by Александр Тихомиров on 2023-10-02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
