c468ff4d975e2b851ed71da598c8b14220734cea
Alexandr Tikhomirov <alexandrtikhomirov@gmail.com>
2023-10-03 13:47:24 +0200

    Added LoggerManager to ShtLoggerDemo-iOS

--- ShtLoggerDemo-iOS/ShtLoggerDemo-iOS/AppDelegate.swift
+++ ShtLoggerDemo-iOS/ShtLoggerDemo-iOS/AppDelegate.swift
@@ -1,19 +1,11 @@
-//
-//  AppDelegate.swift
-//  ShtLoggerDemo-iOS
-//
-//  Created by Александр Тихомиров on 2023-10-02.
-//
-
 import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+
         return true
     }
 
