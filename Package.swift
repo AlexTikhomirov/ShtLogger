2da736c823a2965aa30230dc148f0b7e5935e976
Alexandr Tikhomirov <alexandrtikhomirov@gmail.com>
2023-10-09 15:34:03 +0200

    Next version

--- Package.swift
+++ Package.swift
@@ -12,8 +12,7 @@ let package = Package(
             targets: ["ShtLogger"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+         .package(url: "https://github.com/AlexTikhomirov/ShtLogger.git", from: "1.0.1"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
